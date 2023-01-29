efcf0b595c982ce78ae4a2571910ccd205a11eb8
Vagapova Valeriia <lera.vagapova@gmail.com>
2023-01-29 23:59:31 +0200

    add reducers

--- src/features/counter/todoSlice.js
+++ src/features/counter/todoSlice.js
@@ -3,7 +3,20 @@ import { createSlice } from "@reduxjs/toolkit";
 export const todoSlice = createSlice({
   name: "todo",
   initialState: {
-    todo: [],
+    list: [],
   },
-  reducers: {},
+  reducers: {
+    addItem: (state, action) => {
+      state.list.push(action.payload);
+    },
+    removeItem: (state, action) => {
+      state.list.splice(action.payload, 1);
+    },
+  },
 });
+
+// Exporting action creators for every reducer function
+export const {} = todoSlice.actions;
+
+// Exporting reducer
+export default todoSlice.reducer;
